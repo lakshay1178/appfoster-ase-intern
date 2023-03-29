475ebfb97116f5ea3c9b331bf858e6f57b5c977b
Lakshay <lakshay15012000@gmail.com>
2023-03-29 15:48:18 +0530

    JS issue done

--- task-8-javascript/ind.js
+++ task-8-javascript/ind.js
@@ -15,7 +15,7 @@ const randomColor = () => {
 
     // randomNumber = Math.floor(randomNumber);
     // randomNumber = randomNumber.toString(16);
-    
+
     // let randColor = randomNumber.padStart(6, 0);   
     // return `#${randColor.toUpperCase()}`
     const letters = "0123456789ABCDEF";
@@ -27,6 +27,33 @@ const randomColor = () => {
     return hex;
 }
 
+const check = () => {
+    var valid = true, error = "", field = "";
+
+    const input = inputVal.value;
+    const num = numSplits.value;
+
+    const errorField = document.getElementById("errorField")
+    // inputVal = field 
+
+    // numsplits = error 
+    if (input < num) {
+      valid = false;
+       numSplits.classList.add("err");
+       numSplits.style.backgroundColor = "rgb(246, 213, 213)";
+       errorField.style.color = "red";
+       errorField.innerHTML = "Splits must be lesser than or equal to the entered Number before";
+    } else {
+        numSplits.style.backgroundColor = "";
+        numSplits.classList.remove("err");
+       errorField.innerHTML = "";
+    }
+  
+    
+    return valid;
+
+
+}
 
 
 
@@ -35,24 +62,29 @@ const split = () => {
     const num = numSplits.value;
     let curr = input;
 
-    for (let i = 0; i < num; i++) {
-        
-        const div = document.createElement("div")
-        div.classList.add("split")
-        div.style.backgroundColor = randomColor();
 
-        const len = Math.ceil(curr / (num - i));
-        div.style.flex = len;
-        div.innerHTML = len;
-        resultCont.appendChild(div);
-        curr = curr - len;
-    }
-    inputVal.value = "";
-    numSplits.value = "";
+        for (let i = 0; i < num; i++) {
+
+            const div = document.createElement("div")
+            div.classList.add("split")
+            div.style.backgroundColor = randomColor();
+
+            const len = Math.ceil(curr / (num - i));
+            div.style.flex = len;
+            div.innerHTML = len;
+            resultCont.appendChild(div);
+            curr = curr - len;
+        }
+        inputVal.value = "";
+        numSplits.value = "";
 
 
 
 }
 
+numSplits.addEventListener("input" , check)
 
-btn.addEventListener("click" , split)
+
+btn.addEventListener("click", split)
+
+
